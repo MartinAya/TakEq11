d5de19ce92d29c792b2c8b40386fd10d7586429c
MartinAya <39680899+MartinAya@users.noreply.github.com>
2020-07-01 22:47:19 -0300

    integracion

--- proyecto/src/Tak_eq11.hs
+++ proyecto/src/Tak_eq11.hs
@@ -30,9 +30,13 @@ Funciones de marca sin ninguna implementación útil. Reemplazar por el código 
 a los módulos necesarios.
 -}
 
+data Ficha = Horizontal TakPlayer | Vertical TakPlayer deriving (Eq,Show)
+type Tablero = [Casilla]
 data TakPlayer = WhitePlayer | BlackPlayer deriving (Eq, Show, Enum, Bounded)
 data TakGame = ConstructorTakGame Tablero TakPlayer deriving (Eq,Show)
 data TakAction = Colocar Int Ficha | Mover Int Int Ficha deriving (Eq,Show)
+data Casilla = ConstructorCasilla [Ficha] deriving (Eq,Show)
+
 
 beginning3x3 :: TakGame -- El estado inicial del juego Tak con un tablero de 3x3, con el tablero vacío. 
 beginning3x3 = ConstructorTakGame [ ConstructorCasilla [] | _ <- [1..9]] WhitePlayer
@@ -43,11 +47,13 @@ beginning4x4 = ConstructorTakGame [ ConstructorCasilla [] | _ <- [1..16]] WhiteP
 actions :: TakGame -> [(TakPlayer, [TakAction])] -- La lista debe incluir una y solo una tupla para cada jugador. 
                                                      -- Si el jugador está activo, la lista asociada debe incluir todos sus 
                                                      -- posibles movimientos para el estado de juego dado. Sino la lista debe estar vacía.
+
 actions juego                                    
-    |activo==WhitePlayer = [(BlackPlayer,[]) , (WhitePlayer, [Colocar cas fichaBlanca | cas <- posicionesVacias ]  ) ]
-    |activo==BlackPlayer = [(BlackPlayer, [Colocar cas fichaNegra | cas <- posicionesVacias]  ) , (WhitePlayer,[]) ]
+    |activo==Nothing = [(BlackPlayer,[]),(WhitePlayer,[])]
+    |activo==(Just WhitePlayer) = [(BlackPlayer,[]) , (WhitePlayer, [Colocar cas fichaBlanca | cas <- posicionesVacias ]  ) ]
+    |activo==(Just BlackPlayer) = [(BlackPlayer, [Colocar cas fichaNegra | cas <- posicionesVacias]  ) , (WhitePlayer,[]) ]
         where
-                activo = activePlayer juego                           
+                activo =  activePlayer juego           
                 fichaBlanca = Horizontal WhitePlayer
                 fichaNegra = Horizontal BlackPlayer
                 posicionesVacias = [y | x <- (getTablero juego) ,  y <- [0..(length (getTablero juego) )]  , casillaVacia x]
@@ -56,25 +62,140 @@ actions _ = error "no existis"
 next :: TakGame -> (TakPlayer, TakAction) -> TakGame -- Esta función aplica una acción sobre un estado de juego dado, y retorna 
                                                          -- jugador activo, si el juego está terminado, o si la acción no es realizable. 
 next (ConstructorTakGame casillas WhitePlayer) (jugador, (Colocar cas ficha) ) = ConstructorTakGame (colocar casillas (jugador, (Colocar cas ficha) ) ) (BlackPlayer)                                                    -- el estado resultante. Se debe levantar un error si el jugador dado no es el 
-next _ _ = error "no implementado"   
+next _ _ = error "no implementado" 
+
+casillaVacia :: Casilla -> Bool
+casillaVacia (ConstructorCasilla [] ) = True
+otherwise = False
+
+getTablero :: TakGame -> Tablero
+getTablero (ConstructorTakGame tablero _) = tablero
+    
+setLista :: (Eq a) => [a] -> Int -> a -> [a]
+setLista lista posicion ele
+   |posicion < 0 || posicion > (length lista - 1)= error "indice invalido"
+   |True = [if x==posicion then ele else lista!!x | x <- [0..(length lista-1)] ]
+
+
+
+
 
 result :: TakGame -> [(TakPlayer, Int)]
-result (TakGame f) = zip players (if f then [] else [1, -1]) --TODO
+result (ConstructorTakGame _ _) = zip players (if True then [] else [1, -1]) --TODO
 
-score :: TakGame -> [(TakPlayer, Int)]
-score _ = zip players [0, 0] --TODO
-
-showBoard :: TakGame -> String
-showBoard g = show g --TODO
+--score :: TakGame -> [(TakPlayer, Int)]
+--score _ = zip players [0, 0] --TODO
 
 showAction :: TakAction -> String
 showAction a = show a --TODO
    
 readAction :: String -> TakAction
-readAction = read --TODO
+readAction _ = (Colocar 1 (Horizontal WhitePlayer)) --TODO
+
+-- activePlayer :: TakGame -> Maybe TakPlayer
+-- activePlayer g = listToMaybe [p | (p, as) <- actions g, not (null as)]
+
+-- activePlayer :: TakGame -> TakPlayer -- Esta función determina a cuál jugador le toca mover, dado un estado de juego.
+-- activePlayer (ConstructorTakGame _ WhitePlayer) = WhitePlayer
+-- activePlayer (ConstructorTakGame _ BlackPlayer) = BlackPlayer
 
 activePlayer :: TakGame -> Maybe TakPlayer
 activePlayer g = listToMaybe [p | (p, as) <- actions g, not (null as)]
+
+
+
+apilarFicha :: Casilla -> Ficha -> Casilla
+apilarFicha (ConstructorCasilla fichas) fichaNueva = (ConstructorCasilla (fichaNueva:(fichas)))
+
+colocar :: Tablero -> (TakPlayer, TakAction) -> Tablero
+colocar tablero (WhitePlayer, (Colocar posicion fichaBlanca)) = nuevoTablero
+      where
+            nuevoTablero = setLista tablero (posicion-1) nuevaCasilla
+            laCasilla = tablero!!posicion
+            nuevaCasilla = apilarFicha laCasilla (Horizontal WhitePlayer)
+colocar tablero (BlackPlayer, (Colocar posicion fichaNegra)) = nuevoTablero
+      where
+            nuevoTablero = setLista tablero (posicion-1) nuevaCasilla
+            laCasilla = tablero!!posicion
+            nuevaCasilla = apilarFicha laCasilla (Horizontal BlackPlayer)
+colocar _ _ = error "no implementado"
+
+tableroLleno :: Tablero -> Bool
+tableroLleno tablero = length (filter casillaVacia tablero) == 0
+
+fichaDeArriba :: Casilla -> Ficha
+fichaDeArriba (ConstructorCasilla fichas) = last fichas
+
+getJugadorEnFicha :: Ficha -> TakPlayer
+getJugadorEnFicha (Horizontal jug) = jug
+getJugadorEnFicha (Vertical jug) = jug
+
+
+casillaDeJugador :: TakPlayer -> Casilla -> Bool
+casillaDeJugador jugador casilla = jugador == jug
+        where
+            jug = getJugadorEnFicha (fichaDeArriba casilla)
+        
+posicionesLlenas :: Tablero -> [Int] -> Bool
+posicionesLlenas tablero posiciones = foldr1 (&&) llenas
+        where
+            llenas = [ not (casillaVacia (tablero!!x)) | x <- posiciones]
+
+caminoCompleto :: Tablero -> TakPlayer -> Bool
+caminoCompleto tablero jugador
+
+        --si existiera una funcion magica que tire cada una de estas listas se puede
+        --hacer un map o algo asi con todas
+        -- funcionMagica :: Tablero -> [[Int]]
+        |posicionesLlenas tableroSoloConFichasJugador [1,4,7] = True
+        |posicionesLlenas tableroSoloConFichasJugador [2,5,8] = True
+        |posicionesLlenas tableroSoloConFichasJugador [3,6,9] = True
+
+        |posicionesLlenas tableroSoloConFichasJugador [1,2,3] = True
+        |posicionesLlenas tableroSoloConFichasJugador [4,5,6] = True
+        |posicionesLlenas tableroSoloConFichasJugador [7,8,9] = True
+
+        |posicionesLlenas tableroSoloConFichasJugador [1,4,5,8] = True
+        |posicionesLlenas tableroSoloConFichasJugador [2,5,6,9] = True
+        |posicionesLlenas tableroSoloConFichasJugador [3,6,5,8] = True
+        |posicionesLlenas tableroSoloConFichasJugador [2,5,4,7] = True
+        |posicionesLlenas tableroSoloConFichasJugador [1,2,5,6] = True
+        |posicionesLlenas tableroSoloConFichasJugador [4,5,8,9] = True
+        |posicionesLlenas tableroSoloConFichasJugador [7,8,5,6] = True
+        |posicionesLlenas tableroSoloConFichasJugador [4,5,2,3] = True
+        |True  = False
+        where
+            tableroSoloConFichasJugador = map (vaciarCasillaSiNoEsDeJugador jugador) tablero
+
+vaciarCasillaSiNoEsDeJugador :: TakPlayer -> Casilla -> Casilla
+vaciarCasillaSiNoEsDeJugador jugador casilla
+        |casillaDeJugador jugador casilla = casilla
+        |True = ConstructorCasilla []
+
+showFicha :: Ficha -> String
+showFicha (Horizontal ply) = "Horizontal" ++ "-" ++show (ply) ++ ","
+showFicha (Vertical ply) = "Vertical" ++ "-" ++show (ply) ++ ","
+
+
+--putStr (showGame beginning3x3)
+showCasilla :: Casilla -> String
+showCasilla (ConstructorCasilla []) = " casilla vacia" ++ "\n"
+showCasilla (ConstructorCasilla fichas) = (foldr1 (++) (map showFicha fichas)) ++ "\n"
+    
+
+showTablero :: Tablero -> String
+showTablero tablero = foldr1 (++) (indiceYCasilla)
+        where
+            stringCasillas = map showCasilla tablero
+            stringIndices = map show [1..(length tablero)]
+            indiceYCasilla = zipWith (++) stringIndices stringCasillas
+            --indiceYCasilla = stringIndices
+
+showBoard :: TakGame -> String -- Convierte el estado de juego a un texto que puede ser impreso en la consola para mostrar el tablero y demás información de la partida. 
+showBoard (ConstructorTakGame tablero WhitePlayer) = "Le toca a Blancas " ++ "\n" ++  (showTablero tablero)
+showBoard (ConstructorTakGame tablero BlackPlayer) = "Le toca a Negras "++ "\n" ++ (showTablero tablero)
+    
+
 
 players :: [TakPlayer]
 players = [minBound..maxBound]
@@ -140,5 +261,7 @@ randomAgent player state = do
     else do
        i <- randomRIO (0, (length moves) - 1)
        return (Just (moves !! i))
+
+
 
 -- Fin
